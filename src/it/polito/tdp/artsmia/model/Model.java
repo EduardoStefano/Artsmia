eaf24e51fb8ed62e8fc22e493d9d7263fad8e506
stefa <stefa@DESKTOP-NJ0RTHC>
2019-05-08 18:19:37 +0200

    Punto 1

---
+++ src/it/polito/tdp/artsmia/model/Model.java
@@ -0,0 +1,49 @@
+package it.polito.tdp.artsmia.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.artsmia.db.ArtsmiaDAO;
+
+public class Model {
+	private Graph<ArtObject, DefaultWeightedEdge> graph;
+	private Map<Integer, ArtObject> idMap;
+	
+	public Model() {
+		idMap = new HashMap<Integer, ArtObject>();
+		graph = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+	}
+	
+	public void creaGrafo() {
+		ArtsmiaDAO dao = new ArtsmiaDAO();
+		dao.listObjects(idMap);
+		
+		//aggiungo i vertici
+		Graphs.addAllVertices(graph, idMap.values());
+		
+		//aggiungo gli archi
+		List<Adiacenza> adj = dao.listAdiacenza();
+		for(Adiacenza a : adj) {
+			ArtObject source = idMap.get(a.getO1());
+			ArtObject dest = idMap.get(a.getO2());
+			try {
+				Graphs.addEdge(graph, source, dest, a.getPeso());
+			}
+			catch(Throwable t) {
+				
+			}
+		}
+		
+		System.out.println("Grafo creato: "+ graph.vertexSet().size()+ " Vertici e "+ graph.edgeSet().size()+" Archi;");
+	}
+	
+}
+
+
+
